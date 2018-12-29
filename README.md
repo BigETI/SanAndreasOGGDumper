eea9daf1f12ce89d07d9c976290dc27ab55d318d
BigETI <bigeti.ethem@gmail.com>
2018-12-29 23:34:31 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# San Andreas OGG Dumper
+
+## Description
+San Andreas OGG Dumper is a command line tool to dump OGG data from a GTA San Andreas installation.
+
+## Usage
+Either execute `SanAndreasOGGDumper.exe` and a prompt will show up to enter the audio path of the GTA San Andreas installation, or execute `SanAndreasOGGDumper.exe` with the command line argument to the audio path of the GTA San Andreas installation.
+A directory called `streams` will be created and all the exported OGG files will be available inside the sub directories.
+
+## Dependencies
+- [GTAAudioSharp](https://github.com/GTA-ASM/GTAAudioSharp)
+
+## License
+The license for this project can be found [here](https://github.com/BigETI/SanAndreasOGGDumper/blob/master/LICENSE): https://github.com/BigETI/SanAndreasOGGDumper/blob/master/LICENSE
---
+++ SanAndreasOGGDumper/Program.cs
@@ -0,0 +1,128 @@
+﻿using GTAAudioSharp;
+using System;
+using System.IO;
+
+/// <summary>
+/// San Andreas OGG dumper namespace
+/// </summary>
+namespace SanAndreasOGGDumper
+{
+    /// <summary>
+    /// Program class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = null;
+                if (args.Length > 0)
+                {
+                    path = args[0];
+                }
+                else
+                {
+                    Console.Write("Please specify a path to dump the audio files from (press return to exit): ");
+                    path = Console.ReadLine();
+                }
+                if (path != null)
+                {
+                    if (path.Trim().Length > 0)
+                    {
+                        string streams_directory = Path.Combine(Environment.CurrentDirectory, "streams");
+                        byte[] buffer = new byte[4096];
+                        if (!(Directory.Exists(streams_directory)))
+                        {
+                            Directory.CreateDirectory(streams_directory);
+                        }
+                        using (GTAAudioFiles gta_audio_files = GTAAudio.OpenRead(path))
+                        {
+                            if (gta_audio_files != null)
+                            {
+                                GTAAudioStreamsFile[] streams_audio_files = gta_audio_files.StreamsAudioFiles;
+                                foreach (GTAAudioStreamsFile streams_audio_file in streams_audio_files)
+                                {
+                                    if (streams_audio_file != null)
+                                    {
+                                        string streams_streams_directory = Path.Combine(streams_directory, streams_audio_file.Name);
+                                        try
+                                        {
+                                            if (!(Directory.Exists(streams_streams_directory)))
+                                            {
+                                                Directory.CreateDirectory(streams_streams_directory);
+                                            }
+                                            for (int i = 0; i < streams_audio_file.NumBanks; i++)
+                                            {
+                                                try
+                                                {
+                                                    using (Stream audio_stream = streams_audio_file.Open((uint)i))
+                                                    {
+                                                        if (audio_stream != null)
+                                                        {
+                                                            string audio_file_path = Path.Combine(streams_streams_directory, streams_audio_file.Name + "." + i + ".ogg");
+                                                            if (File.Exists(audio_file_path))
+                                                            {
+                                                                File.Delete(audio_file_path);
+                                                            }
+                                                            using (FileStream file_stream = File.Open(audio_file_path, FileMode.Create))
+                                                            {
+                                                                int len;
+                                                                long audio_stream_length = audio_stream.Length;
+                                                                while ((len = Math.Min((int)(audio_stream_length - audio_stream.Position), buffer.Length)) > 0)
+                                                                {
+                                                                    if (audio_stream.Read(buffer, 0, len) == len)
+                                                                    {
+                                                                        file_stream.Write(buffer, 0, len);
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        break;
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                                catch (Exception e)
+                                                {
+                                                    Console.Error.WriteLine(e);
+                                                }
+                                            }
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            Console.Error.WriteLine(e);
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.Error.WriteLine("Can't open audio directory \"" + path + "\".");
+                                Environment.ExitCode = 3;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Environment.ExitCode = 2;
+                    }
+                }
+                else
+                {
+                    Environment.ExitCode = 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                Environment.ExitCode = -1;
+            }
+        }
+    }
+}
